3c4b4c3d010b71c8e38430fe9e7b635896fdcc5a
MSTHTRxD <geekmurad@gmail.com>
2017-03-10 09:30:55 +0300

    Add project files.

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        public static void Drawstar(int s) {
+            for(int i = 0; i < s; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            string a, b, c;
+            a = "hello";
+
+            b = " my love";
+            c = a + b;
+            c = c.Substring(4, 6);
+            Console.WriteLine(c);
+            Drawstar(66);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
